3836f2d6be76ee7888dce1e2ed56d46783aa5194
Lucas Ruiz <deth_lucas@hotmail.com>
2022-10-10 20:47:23 -0300

    Manipulando pastas

--- src/javaFileReaderProject/Program.java
+++ src/javaFileReaderProject/Program.java
@@ -2,14 +2,17 @@ package javaFileReaderProject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		String path = "c:\\Program1\\in.txt";
 		String pathOut = "c:\\Program1\\out.txt";
 
@@ -27,7 +30,7 @@ public class Program {
 		}
 
 		String[] lines = new String[] { "Good Morning", "Good afternoon", "Good Night" };
-																	//adicionar parametro True para não recriar o arquivo
+		// adicionar parametro True para não recriar o arquivo
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathOut, true))) {
 			for (String line : lines) {
 				bw.write(line);
@@ -37,5 +40,27 @@ public class Program {
 			e.printStackTrace();
 		}
 
+		System.out.println("Enter a folder Path");
+		String strPath = sc.nextLine();
+		File pathFolder = new File(strPath);
+		// Função para Listar somente as pastas
+		File[] folders = pathFolder.listFiles(File::isDirectory);
+		System.out.println("Folders :");
+		for (File folder : folders) {
+			System.out.println(folder);
+
+		}
+
+		// Função para Listar somente os arquivos
+		File[] files = pathFolder.listFiles(File::isFile);
+		System.out.println("FILES :");
+		for (File file:files) {
+			System.out.println(file);
+			
+		}
+		
+		boolean sucess = new File(pathFolder + "\\newFolder").mkdir();
+		
+		sc.close();
 	}
 }
